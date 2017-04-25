de2a55b9504c280c797f7f51b41f031b70c42549
Wasantha Sampath Ranasinghe <wasantha2ra@gmail.com>
2017-04-25 11:45:06 +0530

    Create WasaNumeric.js

---
+++ WasaNumeric.js
@@ -0,0 +1,36 @@
+function Numeric (element,a,b){
+	
+  if(element.length == undefined){
+    element.addEventListener('input', function (prev) {
+      return function (evt) {
+
+          var reg = new RegExp('^\\d{0,'+a+'}(?:\\.\\d{0,'+b+'})?$','ig')
+          if (!reg.test(this.value)) {
+            this.value = prev;
+          }
+          else {
+            prev = this.value;
+          }
+      };
+    }(element.value), false);    
+  }else{
+  	for (var i = 0; i < element.length; i++) {
+
+      element[i].addEventListener('input', function (prev) {
+        return function (evt) {
+
+            var reg = new RegExp('^\\d{0,'+a+'}(?:\\.\\d{0,'+b+'})?$','ig')
+            if (!reg.test(this.value)) {
+              this.value = prev;
+            }
+            else {
+              prev = this.value;
+            }
+        };
+      }(element[i].value), false);
+
+    }
+  }
+	
+
+}
